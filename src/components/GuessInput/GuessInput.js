1a1a7acecd7f001a079a2c16365e8b3600433ef7
peter staal <peterstaal@MBP-van-peter.home>
2023-03-16 15:37:45 +0100

    create guess input

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GuessInput() {
+  return (
+  <form class="guess-input-wrapper">
+    <label for="guess-input">Enter guess:</label>
+    <input id="guess-input" type="text" />
+  </form>);
+}
+
+export default GuessInput;
